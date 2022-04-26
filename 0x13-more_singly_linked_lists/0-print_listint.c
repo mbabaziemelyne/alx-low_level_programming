ae5709f7df2c12bf55c9a418e4d47c402b549a1a
mbabaziemelyne <99616142+mbabaziemelyne@users.noreply.github.com>
2022-04-26 23:41:53 +0200

    Update 0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -15,9 +15,9 @@ size_t print_listint(const listint_t *h)
 	{
 		while (h)
 		{
-			printf("%d\n", h->n);
-			h = h->next;
-			count++;
+		printf("%d\n", h->n);
+		h = h->next;
+		count++;
 		}
 	}
 
